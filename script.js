6ce07be93c341ff63c5f055a4b2b755f91e9e776
gregorydillon <gregorydillon@gmail.com>
2017-04-08 05:09:43 +0000

    single event listener is better

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-var listItems = document.querySelectorAll('li');
+// var listItems = document.querySelectorAll('li');
 
-for(var i=0; i<listItems.length; i++){
-    listItems[i].addEventListener("click", function(event){
-        alert("You just clicked on " + event.target.innerText);
-    });
-}
+// for(var i=0; i<listItems.length; i++){
+//     listItems[i].addEventListener("click", function(event){
+//         alert("You just clicked on " + event.target.innerText);
+//     });
+// }
 
+// OPTION 1: listening on the parent
+var ul = document.querySelector("ul");
+
+// how many event listeners?
+ul.addEventListener("click", function(event){
+    alert("You just clicked on " + event.target.innerText);
+});
